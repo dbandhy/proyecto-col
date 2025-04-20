6b86e8ebcf8ca7f2d43d81cbc5e4c1cbfc3f2060
Imanol Mayo <121578699+ImanolMayoRojas01@users.noreply.github.com>
2025-04-20 00:09:59 -0500

    feat: design partially futbol page feat: create components reafactor: replace header, footer and social-bar components for new form

--- proyecto-col/static/loadComponents.js
+++ proyecto-col/static/loadComponents.js
@@ -1,12 +1,15 @@
-
-function loadComponent(id, file) {
-    fetch(file)
-        .then(response => response.text())
-        .then(data => document.getElementById(id).innerHTML = data)
-        .catch(error => console.error('Error al cargar el archivo:', error));
+async function loadComponent(id, file) {
+  try {
+    const response = await fetch(file)
+    const data = await response.text()
+    const container = document.getElementById(id)
+    container.innerHTML = data
+  } catch (error) {
+    console.error('Error al cargar el archivo:', error)
+  }
 }
 
 // Cargar el header y footer
-loadComponent('main-header', '/components/header.html');
-loadComponent('main-footer', '/components/footer.html');
+loadComponent('main-header', './components/header.html');
+loadComponent('main-footer', './components/footer.html');
 
